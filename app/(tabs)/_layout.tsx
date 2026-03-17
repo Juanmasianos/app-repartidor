a086c81e078c9e7bdbd56aa3d1b011e2ca13882a
unknown <gonzcarjuan06@gmail.com>
2026-03-17 21:19:09 +0000

    login and the app separated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,27 +7,25 @@ import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarStyle: { backgroundColor: "#009B48" },
         headerShown: false,
-        tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: () => <IconSymbol size={28} name="house.fill" color={"#FED000"} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: () => <IconSymbol size={28} name="paperplane.fill" color={"#FED000"} />,
         }}
       />
     </Tabs>
